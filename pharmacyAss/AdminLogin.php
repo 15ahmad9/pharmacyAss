e3a0a24ff92e11553869953cc97653f73ed0c228
Ahmad Ghanem <109623987+15ahmad9@users.noreply.github.com>
2023-01-24 23:59:05 +0300

    Update AdminLogin.php

--- pharmacyAss/AdminLogin.php
+++ pharmacyAss/AdminLogin.php
@@ -23,8 +23,6 @@
 
   <title>Admin Login</title>
 
-
-
   <link rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous"/>
 </head>
 
@@ -81,7 +79,6 @@ width: 180px;
                 
     </div>
   
-
 
 </body>
 
